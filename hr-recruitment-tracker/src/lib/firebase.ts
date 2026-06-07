ce3bf7c3451f6789fb183fa3f338a54e38efa0c8
anhlm2005yrc-hue <anhlm2005.yrc@gmail.com>
2026-06-07 17:24:23 +0700

    Update firebase.ts

--- hr-recruitment-tracker/src/lib/firebase.ts
+++ hr-recruitment-tracker/src/lib/firebase.ts
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY || "AIzaSyDummyKey_ReplaceWithYourOwn",
+  apiKey: (import.meta as any).env.VITE_FIREBASE_API_KEY || "AIzaSyCcWS-z1jYPgDzVS70vVB7qrTSZ1CyM9Ko",
   authDomain: "hr-recruitment-tracker-419b6.firebaseapp.com",
   databaseURL: "https://hr-recruitment-tracker-419b6-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hr-recruitment-tracker-419b6",
